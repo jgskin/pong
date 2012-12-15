39be52daf454e81a931aebf9d71eced3fdcce14a
Jessé Alves Galdino <galdino.jesse@gmail.com>
2012-12-15 19:39:33 -0200

    [feature] Draw a board
    Main class for Pong game
    Board class
    Board configuration

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,22 @@
 requirejs.config({
-    baseUrl: 'scripts',
+    baseUrl: "scripts",
     shim: {
-        'lib/backbone': {
-            deps: ['lib/underscore', 'lib/jquery'],
-            exports: 'Backbone'
+        "lib/backbone": {
+            deps: ["lib/underscore", "lib/jquery"],
+            exports: "Backbone"
         }
     }
 });
 
-requirejs(['src/app'], function (app) {
+requirejs(["src/app"], function (app) {
 
+    var context = document.getElementById("myCanvas").getContext("2d");
+
+    var board = new app.Board({
+        context: context,
+        position: {"x": 50, "y": 50},
+        size: {"w": 700, "h": 400}
+    });
+
+    board.draw();
 });
--- scripts/src/app.js
+++ scripts/src/app.js
@@ -1,3 +1,52 @@
 define(['lib/backbone'], function (Backbone) {
 
+// Main object
+var Pong = Backbone.Model.extend({
+
+    // model attributes
+    defaults: {
+
+        // the canvas context
+        "context": null,
+
+        // object positions
+        "position": {"x": 0, "y": 0},
+
+        // object size
+        "size": {"w": 0, "h": 0}
+
+    },
+
+    // Draws the object
+    draw: function  () {},
+
+    // Action performed on hit
+    on_hit: function  (object) {},
+
+    // Get the posisitions ocupied by the object
+    get_area: function () {},
+
+    // Test the hit with the another_object for the current position
+    hit_test: function (another_object) {}
+
+});
+
+// Board
+var Board = Pong.extend({
+
+    draw: function  () {
+        var context = this.get("context"),
+            position = this.get("position"),
+            size = this.get("size");
+
+        context.strokeRect(position.x, position.y, size.w, size.h);
+    }
+
+});
+
+return {
+    Pong: Pong,
+    Board: Board
+}
+
 });

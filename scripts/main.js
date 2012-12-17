f6f52e02e1f42c2c375c804b171396accc00f2bb
Jessé Alves Galdino <galdino.jesse@gmail.com>
2012-12-17 13:39:27 -0200

    [feature] Player test

--- scripts/main.js
+++ scripts/main.js
@@ -10,13 +10,26 @@ requirejs.config({
 
 requirejs(["src/app"], function (app) {
 
+    // canvas 2d context
     var context = document.getElementById("myCanvas").getContext("2d");
 
+    // configure the board
     var board = new app.Board({
         context: context,
         position: {"x": 50, "y": 50},
-        size: {"w": 700, "h": 400}
+        size: {"w": 200, "h": 200}
     });
 
+    // draws the board
     board.draw();
+
+    // main event
+    $(document).ready(function(){
+        $(document).keydown(function(event){
+            board.get('players').forEach(function  (player) {
+                player.trigger("movement_call", event.keyCode);
+            });
+        });
+    });
+
 });
